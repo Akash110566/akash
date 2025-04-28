287e3eeb5e2e355314c5e8e1c2999dfdabb350de
Akash110566 <akashb22@karunya.edu.in>
2025-04-28 07:20:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const studentModel = require('./shalo')
+const studentModel = require('./Students')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/akash')
-    .then(() => console.log('DB Connected'))
+mongoose.connect('mongodb://127.0.0.1:27017/College')
+
+    .then(() => console.log('DB connected'))
     .catch(err => console.log(err))
 
 app.post('/savestudent', async (req, res) => {
     try {
         console.log(req.body)
         await studentModel.create(req.body)
-        res.json({ message: 'Student Added Sucessfully' })
+        res.json({ message: 'Student  Added Successfully' })
     }
     catch (error) {
         res.json(error)
@@ -25,13 +26,13 @@ app.post('/savestudent', async (req, res) => {
 app.get('/getstudents', async (req, res) => {
     try {
         const students = await studentModel.find()
-        res.json(students)
+        res.json(students);
     } catch (error) {
-        res.json({error:error.message })
+        res.status(500).json({ error: error.message })
     }
-})
+});
+
 const PORT = 8000
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} `)
+    console.log(`Server running on port ${PORT}`)
 })
-
