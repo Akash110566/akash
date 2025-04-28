6a8b2fd31fa48d8897388eaa30b033b967a38497
Akash110566 <akashb22@karunya.edu.in>
2025-04-28 07:16:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const studentModel = require('./shalo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/akash')
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err))
+
+app.post('/savestudent', async (req, res) => {
+    try {
+        console.log(req.body)
+        await studentModel.create(req.body)
+        res.json({ message: 'Student Added Sucessfully' })
+    }
+    catch (error) {
+        res.json(error)
+    }
+})
+
+app.get('/getstudents', async (req, res) => {
+    try {
+        const students = await studentModel.find()
+        res.json(students)
+    } catch (error) {
+        res.json({error:error.message })
+    }
+})
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} `)
+})
+
