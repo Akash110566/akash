451290d84e69095a6d716a45ef81a911ff1da4fa
Akash110566 <akashb22@karunya.edu.in>
2025-04-28 07:21:31 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [dept, setDept] = useState('')
   const [students, setStudents] = useState([])
 
+
   const saveStudent = async (event) => {
     event.preventDefault()
     try {
@@ -23,64 +24,68 @@ function App() {
     }
   }
 
-  const fetchStudents = async (event) => {
+  const fetchStudents = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/getstudents')
-      setStudents(response.data)
+      const response = await axios.get('http://localhost:8000/getstudents');
+      setStudents(response.data);
     } catch (err) {
-      console.error(err)
-      alert('Failed to fetch student')
+      console.error(err);
+      alert('Failed to fetch students');
     }
   }
 
 
   return (
     <div>
-      <h1>Student details</h1>
+      <h1>Save Student Details</h1>
       <form onSubmit={saveStudent}>
         <div>
           <label>Name:</label>
           <input type="text" onChange={(e) => setName(e.target.value)} />
           <br />
-          <label>Reg no:</label>
-          <input type="text" onChange={(e) => setRegno(e.target.value)} />
+          <label>Regno:</label>
+          <input type="number" onChange={(e) => setRegno(e.target.value)} />
           <br />
           <label>CGPA:</label>
-          <input type="text" onChange={(e) => setCGPA(e.target.value)} />
+          <input type="number" onChange={(e) => setCGPA(e.target.value)} />
           <br />
           <label>Dept:</label>
           <input type="text" onChange={(e) => setDept(e.target.value)} />
           <br />
-          <button>Save Student</button>
+          <br />
+          <button>Save Student </button>
         </div>
       </form>
+      <br />
+      <button onClick={fetchStudents}>view</button>
 
-      <button onClick={fetchStudents}>View details</button>
+      <h1>All STUDENTS</h1>
 
-      <h2>ALL STUDENTS</h2>
       <table>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Reg no</th>
+            <th>Regno</th>
             <th>CGPA</th>
             <th>Dept</th>
           </tr>
         </thead>
-
         <tbody>
           {students.map((student, index) => (
-            <tr key={index} >
-              <td>{student.name}</td>
-              <td>{student.regno}</td>
-              <td>{student.cgpa}</td>
-              <td>{student.dept}</td>
+            <tr key={index}>
+              <td className="border border-gray-400 px-4 py-2">{student.name}</td>
+              <td className="border border-gray-400 px-4 py-2">{student.regno}</td>
+              <td className="border border-gray-400 px-4 py-2">{student.cgpa}</td>
+              <td className="border border-gray-400 px-4 py-2">{student.dept}</td>
             </tr>
           ))}
-
         </tbody>
       </table>
+
+
+
     </div>
+
   )
 }
 
